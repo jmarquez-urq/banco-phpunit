87d1660766a67a70dc92c42a1c327fe89c693de6
jmarquez-urq <jmarquez.urquiza@gmail.com>
2021-09-15 15:38:48 -0300

    Esqueleto de las clases de testing

---
+++ tests/CajaAhorroTest.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'CuentaTest.php';
+
+class CajaAhorroTest extends CuentaTest
+{
+}
---
+++ tests/CuentaCorrienteTest.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'CuentaTest.php';
+class CuentaCorrienteTest extends CuentaTest
+{
+}
